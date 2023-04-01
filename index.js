851b00f389842c43812903de7c04abca48c413a6
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-01 15:38:46 -0500

    add question prompts

--- index.js
+++ index.js
@@ -1,10 +1,108 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: 'license'
+        message: 'Select a license type: '
+        type: 'list'
+        choices: ''
+        validate: (answer) => {
+            if (answer === "") {
+                return console.log("Enter a valid title for your project");
+            }
+            return true;
+        }
+    },
+    { 
+        name: 'title'
+        message: 'What is the title?'
+        type: 'input'
+        validate: (answer) => {
+            if (answer === "") {
+                return console.log("Enter a valid title for your project");
+            }
+            return true;
+        }
+    },
+    {
+        name: 'description'
+        message: 'Describe the application:'
+        type: 'input'
+        validate: (answer) => {
+            if (answer === "") {
+                return console.log("Enter a valid title for your project");
+            }
+            return true;
+        }
+    },
+    {
+        name: 'install'
+        message: 'How do you install the application?'
+        type: 'input'
+        validate: (answer) => {
+            if (answer === "") {
+                return console.log("Enter a valid title for your project");
+            }
+            return true;
+        }
+    },
+    {
+        name: 'usage'
+        message: "How is the application used?"
+        type: 'input'
+        validate: (answer) => {
+            if (answer === "") {
+                return console.log("Enter a valid title for your project");
+            }
+            return true;
+        }
+    },
+    {
+        name: 'contributing'
+        message: 'How can others contribute?'
+        type: 'input'
+        validate: (answer) => {
+            if (answer === "") {
+                return console.log("Enter a valid title for your project");
+            }
+            return true;
+        }
+    },
+    {
+        name: 'tests'
+        message: 'How can you test the application?'
+        type: 'input'
+        validate: (answer) => {
+            if (answer === "") {
+                return console.log("Enter a valid title for your project");
+            }
+            return true;
+        }
+    },
+    {
+        name: 'username'
+        message: 'What is your GitHub username?'
+        type: 'input'
+        validate: (answer) => {
+            if (answer === "") {
+                return console.log("Enter a valid title for your project");
+            }
+            return true;
+        }
+    },
+    {
+        name: 'email'
+        message: 'What is your email?'
+        type: 'input'
+        validate: (answer) => {
+            if (answer === "") {
+                return console.log("Enter a valid title for your project");
+            }
+            return true;
+        }
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
