0c3dfa7bbca5ffafc54279674fcad2f7edd40127
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-06 10:51:54 -0500

    add break between email and github

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -118,7 +118,8 @@ function generateMarkdown(data) {
 
   ## Questions
   Contact me with any additional questions:
-  [https://github.com/${data.username}](https://github.com/${data.username})
+  [https://github.com/${data.username}]
+  (https://github.com/${data.username})
   ${data.email}
 `;
 }
